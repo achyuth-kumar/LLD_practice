fe042c62fa96657045e8181ce7bdd4ba2bef3e8f
achyuth-kumar <achyuthkumar773@gmail.com>
2025-08-24 21:54:41 +0530

    24-8-2025 updated 2

--- BridgeDesignPattern1/Main.java
+++ BridgeDesignPattern1/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String [] args) {
+        LivingThings dog=new Dog(new AirBreatheImpl());
+        dog.breatheProcess();
+
         LivingThings fish=new Fish(new WaterBreatheImpl());
         fish.breatheProcess();
     }
