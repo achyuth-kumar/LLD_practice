44f8af4f036e38f26cdd22b3806945169b1bb083
achyuth <achyuth@example.com>
2025-10-05 17:48:06 +0530

    5-10-2025 updated

--- Cache1/LFUCache.java
+++ Cache1/LFUCache.java
@@ -1,23 +1,33 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LFUCache implements Cache {
-    HashMap<Integer,Integer> cache = new HashMap<>();
-    HashMap<Integer,Integer> freq=new HashMap<>();
+    HashMap<Integer,List<Integer>> cache = new HashMap<>();
     @Override
     public void insertKey(int key, int value) {
-        freq.put(key,freq.getOrDefault(key,0)+1);
-        cache.put(key,value);   
+        List<Integer> list=new ArrayList<>();
+        if(cache.containsKey(key)) {
+            list=cache.get(key);
+            list.set(0,list.get(0)+1);
+            list.set(1,value);
+        }
+        else {
+            list.add(0);
+            list.add(value);
+        }
+        cache.put(key,list);   
     }
 
     @Override
     public int getValue() {
         if (cache.isEmpty()) return -1;
         int res=0,mx=Integer.MAX_VALUE;
-        for(Map.Entry<Integer,Integer> entry : freq.entrySet()) {
-            if(entry.getValue()<mx) {
-                res=entry.getKey();
-                mx=entry.getValue();
+        for(List<Integer> entry : cache.values()) {
+            if(entry.get(0)<mx) {
+                res=entry.get(1);
+                mx=entry.get(0);
             }
         }
         return res;
@@ -25,8 +35,8 @@ public class LFUCache implements Cache {
 
     @Override
     public void displayCache() {
-        for (Map.Entry<Integer,Integer>entry : cache.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        for (Map.Entry<Integer,List<Integer>>entry : cache.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue().get(1));
         }
     }
 }
