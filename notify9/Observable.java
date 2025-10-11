fde62e684557db64507be91dc9e65545bfb0a811
achyuth-kumar <achyuthkumar773@gmail.com>
2025-10-11 17:45:14 +0530

    11-10-2025

--- notify9/Observable.java
+++ notify9/Observable.java
@@ -4,5 +4,5 @@ public interface Observable {
     public void addItem(Integer id,String productName);
     public void removeItem(Integer id);
     public void notification(List<NotificationServiceImpl> notificationServiceList);
-    public void displayCartItems();
+    public void displayCartItems(); 
 }
