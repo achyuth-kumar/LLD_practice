e893e1ee8df5ef814234927174a27a1d02b9a428
achyuth-kumar <achyuthkumar773@gmail.com>
2025-12-06 09:22:10 +0530

    6-12-2025

---
+++ Chat2/User.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class User implements Chat {
+
+    @Override
+    public void sendMessage() {
+        
+    }
+
+    @Override
+    public void deleteMessage() {
+
+    }
+
+    @Override
+    public void pinMessage() {
+
+    }
+
+    @Override
+    public List<String> getMessages() {
+        return List.of();
+    }
+}
