caa4c5915ccb4819b17fb65c19820d80cc829e98
achyuth-kumar <achyuthkumar773@gmail.com>
2026-02-13 07:36:59 +0530

    13-2-2026

---
+++ SnakeAndLadder18/Main.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String [] args) {
+        Player playerOne=new Player(1,"Achyuth",new Pair<>(0,0));
+        Player playerTwo=new Player(2,"Kumar",new Pair<>(0,0));
+        HashMap<Pair<Integer,Integer>,Pair<Integer,Integer>> snakes=new HashMap<>();
+        snakes.put(new Pair<>(2,0),new Pair<>(1,0));
+        HashMap<Pair<Integer,Integer>,Pair<Integer,Integer>> ladders=new HashMap<>();
+        ladders.put(new Pair<>(1,1),new Pair<>(2,1));
+        Game snakeAndLadder=new GameServiceImpl(playerOne,playerTwo,snakes,ladders);
+        snakeAndLadder.startGame();
+    }
+}
---
+++ SnakeAndLadder18/Player.java
@@ -0,0 +1,36 @@
+public class Player {
+    public Integer id;
+    public String name;
+    public Pair<Integer,Integer> currentPosition;
+
+    public Player(Integer id, String name, Pair<Integer, Integer> currentPosition) {
+        this.id = id;
+        this.name = name;
+        this.currentPosition = currentPosition;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Pair<Integer, Integer> getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(Pair<Integer, Integer> currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
+
+}
