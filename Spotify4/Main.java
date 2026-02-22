f44b3e8af932b11f4ae6e6708d056b749735329b
achyuth-kumar <achyuthkumar773@gmail.com>
2026-02-22 15:52:51 +0530

    22-2-2026 updated

---
+++ Spotify4/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String [] args) {
+        User user=new User(1,"Achyuth");
+        Song song1=new Song(1,"A1");
+        Song song2=new Song(2,"A2");
+        Song song3=new Song(3,"A3");
+        Song song4=new Song(4,"A4");
+        Song song5=new Song(5,"A5");
+        PlayList spotify=new PlayListServiceImpl();
+        spotify.addSong(song1);
+        spotify.addSong(song2);
+        spotify.addSong(song3);
+        spotify.addSong(song4);
+        spotify.addSong(song5);
+        spotify.viewSongs();
+        System.out.println();
+        spotify.reorderSong(song5,2.0,3.0);
+        spotify.viewSongs();
+    }
+}
