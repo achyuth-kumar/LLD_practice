429f45c37640be9c608d6c90df97937c92203a9d
achyuth-kumar <achyuthkumar773@gmail.com>
2026-02-22 13:13:22 +0530

    22-2-2026 updated

---
+++ SnakeAndLadder19/GameServiceImpl.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Random;
+
+public class GameServiceImpl implements Game{
+    Player playerOne,playerTwo;
+    HashMap<Pair<Integer,Integer>,Pair<Integer,Integer>> snakes,ladders;
+    Integer [][]board;
+    Integer m,n;
+    public GameServiceImpl(Player playerOne, Player playerTwo, HashMap<Pair<Integer, Integer>, Pair<Integer, Integer>> snakes, HashMap<Pair<Integer, Integer>, Pair<Integer, Integer>> ladders) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+        this.snakes = snakes;
+        this.ladders = ladders;
+        this.m=3;
+        this.n=3;
+        this.board=new Integer[m][n];
+    }
+
+    @Override
+    public void startGame() {
+        String Winner=new String();
+        boolean turn=true;
+        Random random=new Random();
+        Pair<Integer,Integer> currentPosition;
+        while(true) {
+            Integer startX=random.nextInt(m);
+            Integer startY=random.nextInt(n);
+            board[startX][startY]=turn?1:2;
+            currentPosition=new Pair<>(startX,startY);
+            if(turn) {
+                processTurn(playerOne,currentPosition);
+            }
+            else {
+                processTurn(playerTwo,currentPosition);
+            }
+            if(startX==(m-1) && startY==(n-1)) {
+                Winner=turn? playerOne.getName() : playerTwo.getName();
+                break;
+            }
+            turn=!turn;
+        }
+        System.out.println("Winner : "+Winner);
+    }
+
+    @Override
+    public void processTurn(Player player,Pair<Integer,Integer> currentPosition) {
+        if(!snakes.containsKey(currentPosition) && !ladders.containsKey(currentPosition)) {
+            player.setCurrentPosition(currentPosition);
+            return ;
+        }
+        if(snakes.containsKey(currentPosition)) {
+            player.setCurrentPosition(snakes.get(currentPosition));
+            processTurn(player,player.getCurrentPosition());
+        }
+        if(ladders.containsKey(currentPosition)) {
+            player.setCurrentPosition(ladders.get(currentPosition));
+            processTurn(player,player.getCurrentPosition());
+        }
+    }
+}
---
+++ SnakeAndLadder19/Player.java
@@ -0,0 +1,38 @@
+public class Player {
+
+
+    Integer id;
+    String name;
+    Pair<Integer,Integer> currentPosition;
+
+    public Player(Integer id, String name, Pair<Integer, Integer> currentPosition) {
+        this.id = id;
+        this.name = name;
+        this.currentPosition = currentPosition;
+    }
+    public Pair<Integer, Integer> getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(Pair<Integer, Integer> currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
