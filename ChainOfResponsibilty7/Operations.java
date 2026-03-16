10db2db606be3c6923de7346ba23c5d1c9a114b6
achyuth-kumar <achyuthkumar773@gmail.com>
2026-03-16 07:49:01 +0530

    16-3-2026

---
+++ ChainOfResponsibilty7/Operations.java
@@ -0,0 +1,9 @@
+public abstract class Operations {
+    Operations next;
+    public Operations(Operations next) {
+        this.next = next;
+    }
+
+    public abstract void processTransactions(Integer amount);
+}
+
