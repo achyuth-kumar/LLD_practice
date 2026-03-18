06b84bf3150c46ff999fd2970a9c3065110be510
achyuth-kumar <achyuthkumar773@gmail.com>
2026-03-18 07:41:31 +0530

    18-3-2026

---
+++ WeightedRondRobin10/Main.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String [] args) {
+        List<String> servers=List.of("S1","S2","S3");
+        List<Integer> weights=List.of(5,1,1);
+        WeightedRoundRobin loadBalancer=new WeightedRoundRobinServiceImpl(servers,weights);
+        for(int i=0;i<7;i++) {
+            System.out.println(loadBalancer.getNextServer());
+        }
+    }
+}
---
+++ WeightedRondRobin10/WeightedRoundRobinServiceImpl.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class WeightedRoundRobinServiceImpl implements WeightedRoundRobin{
+
+    List<String> servers;
+    List<Integer> weights;
+    Integer currentIndex,currentWeight;
+    public WeightedRoundRobinServiceImpl(List<String> servers, List<Integer> weights) {
+        this.currentIndex=-1;
+        this.currentWeight=0;
+        this.servers = servers;
+        this.weights = weights;
+    }
+
+    @Override
+    public String getNextServer() {
+        while(true) {
+            currentIndex=(currentIndex+1)%servers.size();
+            if(currentIndex==0) {
+                currentWeight--;
+                if(currentWeight<=0) {
+                    currentWeight=weights.stream().max(Integer::compare).orElse(0);
+                }
+            }
+            if(weights.get(currentIndex)>=currentWeight) {
+                return servers.get(currentIndex);
+            }
+        }
+    }
+}
