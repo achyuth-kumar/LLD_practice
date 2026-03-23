397a7f5381c01efae4798684fce3bf48ea8e4bb6
achyuth-kumar <achyuthkumar773@gmail.com>
2026-03-24 05:17:05 +0530

    24-3-2026

---
+++ DecoratorDesignPattern11/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String [] args) {
+        BasePizza basePizza=new ExtraCheese(new VegPizza());
+        System.out.println(basePizza.cost());
+
+        basePizza=new ExtraMushroom(new ExtraCheese(new NonVegPizza()));
+        System.out.println(basePizza.cost());
+
+    }
+}
