86278cc00c5ed6fd8702af833ab4fba1863ebae7
achyuth-kumar <achyuthkumar773@gmail.com>
2026-03-27 08:27:35 +0530

    27-3-2026

---
+++ CommandDesignPattern/TurnOffServiceImpl.java
@@ -0,0 +1,16 @@
+public class TurnOffServiceImpl implements ICommand{
+
+    AirConditionServiceImpl airConditionService;
+    public TurnOffServiceImpl(AirConditionServiceImpl airConditionService) {
+        this.airConditionService = airConditionService;
+    }
+    @Override
+    public void execute() {
+        airConditionService.turnOff();
+    }
+
+    @Override
+    public void undo() {
+        airConditionService.turnOn();
+    }
+}
