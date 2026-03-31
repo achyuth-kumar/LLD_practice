8c5dc1d70b7e78a5d638ca7cf45fceeb99c077ff
achyuth-kumar <achyuthkumar773@gmail.com>
2026-03-31 07:05:24 +0530

    31-3-2026

---
+++ Chess21/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String [] args) {
+        Player whitePlayer=new Player(1,"Achyuth",Colour.white);
+        Player blackPlayer=new Player(2,"Kumar",Colour.black);
+        Game chess=new GameServiceImpl(whitePlayer,blackPlayer,new Cell[8][8],8,8);
+        chess.startGame(new Pawn(Colour.white),1,1,2,2);
+
+    }
+}
