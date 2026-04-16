e0c980b7540777e7cf5c5597a120020b14c419c9
achyuth-kumar <achyuthkumar773@gmail.com>
2026-04-16 06:25:27 +0530

    16-4-2026

---
+++ ChainOfResponsibility11/FiveHundredHandler.java
@@ -0,0 +1,20 @@
+public class FiveHundredHandler extends Operations{
+    public FiveHundredHandler(Operations next) {
+        super(next);
+    }
+
+    @Override
+    public void processTransactions(Integer amount) {
+        if(amount>=500) {
+            Integer notes=amount/500;
+            Integer remainder=amount%500;
+            System.out.println("Five Hundred notes : "+notes);
+            if(remainder>0 && next!=null) {
+                next.processTransactions(remainder);
+            }
+        }
+        else {
+            next.processTransactions(amount);
+        }
+    }
+}
