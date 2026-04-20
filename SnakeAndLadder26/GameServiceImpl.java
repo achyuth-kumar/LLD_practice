b4a225526c24ee85577d98e94f65f65806599e82
achyuth-kumar <achyuthkumar773@gmail.com>
2026-04-20 06:53:10 +0530

    20-4-2026

---
+++ SnakeAndLadder26/GameServiceImpl.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+import java.util.Random;
+
+public class GameServiceImpl implements Game{
+    Integer[][] board;
+    Integer m,n;
+    Player playerOne,playerTwo;
+    HashMap<Pair<Integer,Integer>,Pair<Integer,Integer>> snakes,ladders;
+    public GameServiceImpl(Integer[][] board, Integer m, Integer n, Player playerOne, Player playerTwo, HashMap<Pair<Integer, Integer>, Pair<Integer, Integer>> snakes, HashMap<Pair<Integer, Integer>, Pair<Integer, Integer>> ladders) {
+        this.board = board;
+        this.m = m;
+        this.n = n;
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+        this.snakes = snakes;
+        this.ladders = ladders;
+    }
+    @Override
+    public void startGame() {
+        boolean turn=true;
+        Random random=new Random();
+        while(true) {
+            Integer startX=random.nextInt(m);
+            Integer startY=random.nextInt(n);
+            board[startX][startY]=turn?1:2;
+            Pair<Integer,Integer> currentPosition=new Pair<>(startX,startY);
+            if(turn) {
+                processTurn(playerOne,currentPosition);
+            }
+            else {
+                processTurn(playerTwo,currentPosition);
+            }
+            if(startX==(m-1) && startY==(n-1)) {
+                System.out.println("Winner : "+(turn?playerOne.getName() : playerTwo.getName()));
+                break;
+            }
+            turn=!turn;
+        }
+    }
+
+    @Override
+    public void processTurn(Player player, Pair<Integer, Integer> currentPosition) {
+        if(!snakes.containsKey(currentPosition) && !ladders.containsKey(currentPosition)) {
+            player.setCurrentPosition(currentPosition);
+            return ;
+        }
+        if(snakes.containsKey(currentPosition)) {
+            player.setCurrentPosition(snakes.get(currentPosition));
+            processTurn(player,player.getCurrentPosition());
+        }
+        if(ladders.containsKey(currentPosition)) {
+            player.setCurrentPosition(ladders.get(currentPosition));
+            processTurn(player,player.getCurrentPosition());
+        }
+    }
+}
