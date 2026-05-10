4ab9112d54cc4911578869a6f583608a52f7ea2a
achyuth-kumar <achyuthkumar773@gmail.com>
2026-05-10 06:48:07 +0530

    10-5-2026

---
+++ CommandDesignPattern18/AirConditionServiceImpl.java
@@ -0,0 +1,9 @@
+public class AirConditionServiceImpl {
+    public void turnOn() {
+        System.out.println("Ac turned ON");
+    }
+    public void turnOff() {
+        System.out.println("Ac turned OFF");
+    }
+}
+
