936d1d67e59191ceac70033c4ad97ef14bc31d76
achyuth-kumar <achyuthkumar773@gmail.com>
2026-05-11 06:44:30 +0530

    11-5-2026

---
+++ CommandDesignPattern19/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+         MyRemote myRemote=new MyRemote(new TurnOnServiceImpl(new AirConditionServiceImpl()));
+         myRemote.pressButton();
+         myRemote.pressUndo();
+
+        myRemote=new MyRemote(new TurnOffServiceImpl(new AirConditionServiceImpl()));
+        myRemote.pressButton();
+        myRemote.pressUndo();
+
+    }
+}
