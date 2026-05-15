5312d2cc6a31fe8e1e3835c2236fc5d297046a70
achyuth-kumar <achyuthkumar773@gmail.com>
2026-05-15 06:41:22 +0530

    15-5-2026

---
+++ ChainOfResponsibility22/HundredHandler.java
@@ -0,0 +1,21 @@
+public class HundredHandler extends Operations{
+    public HundredHandler(Operations next) {
+        super(next);
+    }
+
+    @Override
+    public void processTransactions(Integer amount) {
+        if(amount>=100) {
+            Integer notes=amount/100;
+            Integer remainder=amount%100;
+            System.out.println("Hundred notes : "+ notes);
+            if(remainder!=0 && next!=null) {
+                next.processTransactions(remainder);
+            }
+        }
+        else {
+            next.processTransactions(amount);
+        }
+
+    }
+}
---
+++ ChainOfResponsibility22/Operations.java
@@ -0,0 +1,8 @@
+public abstract class Operations {
+
+    Operations next;
+    public Operations(Operations next) {
+        this.next = next;
+    }
+    public abstract  void processTransactions(Integer amount);
+}
