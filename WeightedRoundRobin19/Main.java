072451a3c659d394eebc30bcda4ba8a6995ac98f
achyuth-kumar <achyuthkumar773@gmail.com>
2026-05-24 07:32:24 +0530

    24-5-2026

---
+++ WeightedRoundRobin19/Main.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String>servers=List.of("S1","S2","S3");
+        List<Integer>weights=List.of(5,1,1);
+        Integer currentIndex=-1,currentWeight=0;
+        WeightedRoundRobin loadbalancer=new WeighterRoundRobinServiceImpl(servers,weights,currentIndex,currentWeight);
+        for(int i=0;i<7;i++) {
+            System.out.println(loadbalancer.getNextServer());
+        }
+    }
+}
