75fc6e4706ad0c16f193c12f909a56061262ffef
achyuth-kumar <achyuthkumar773@gmail.com>
2026-06-06 08:19:49 +0530

    6-6-2026

---
+++ ArthematicExpression10/Number.java
@@ -0,0 +1,13 @@
+public class Number implements ArthematicExpression{
+
+    Integer n;
+
+    public Number(Integer n) {
+        this.n = n;
+    }
+
+    @Override
+    public Integer evaluate() {
+        return this.n;
+    }
+}
