fcad90dfcfaf03f07802eb9c3907b29705b122d3
achyuth-kumar <achyuthkumar773@gmail.com>
2026-07-06 06:54:04 +0530

    6-7-2026

---
+++ Notify25/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<NotificationServiceImpl> notificationServiceList=List.of(new EmailNotificationServiceImpl(),new MessageNotificationServiceImpl());
+        Observable observable=new ObservableServiceImpl(notificationServiceList);
+        observable.addItem("demo");
+        observable.removeItem("demo");
+        observable.NotifyMe();
+    }
+}
---
+++ Notify25/ObservableServiceImpl.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class ObservableServiceImpl implements Observable{
+
+
+    List<NotificationServiceImpl> notificationServiceList;
+    public ObservableServiceImpl(List<NotificationServiceImpl> notificationServiceList) {
+        this.notificationServiceList = notificationServiceList;
+    }
+    @Override
+    public void addItem(String item) {
+        System.out.println(item+" : got added into cart");
+    }
+
+    @Override
+    public void removeItem(String item) {
+        System.out.println(item+" : removed from cart");
+    }
+
+    @Override
+    public void NotifyMe() {
+        for(Observer notificationService : notificationServiceList) {
+            notificationService.update();
+        }
+    }
+}
