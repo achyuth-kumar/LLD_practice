4d7098be332d64dac32bb3f1c04009314ffdbf85
achyuth-kumar <achyuthkumar773@gmail.com>
2026-07-15 06:52:04 +0530

    15-7-2026

---
+++ TicTacToe27/GameServiceImpl.java
@@ -0,0 +1,62 @@
+import javax.management.monitor.StringMonitor;
+import javax.swing.*;
+import java.util.Random;
+
+public class GameServiceImpl implements Game{
+    Piece [][]board;
+    Player playerOne,playerTwo;
+    Integer m,n;
+    public GameServiceImpl(Piece[][] board, Player playerOne, Player playerTwo, Integer m, Integer n) {
+        this.board = board;
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+        this.m = m;
+        this.n = n;
+    }
+
+    @Override
+    public void startGame() {
+        Random random=new Random();
+        boolean turn=true;
+        String winner=new String();
+        while(true) {
+            Integer startX=random.nextInt(m);
+            Integer startY=random.nextInt(n);
+            board[startX][startY]=turn?playerOne.getPiece():playerTwo.getPiece();
+            for(int i=0;i<n;i++) {
+                if(board[i][0]!=null && board[i][0]==board[i][1] && board[i][1]==board[i][2]) {
+                    winner= turn?playerOne.getName():playerTwo.getName();
+                    break;
+                }
+                if(board[0][i]!=null && board[0][i]==board[1][i] && board[1][i]==board[2][i]) {
+                    winner= turn?playerOne.getName():playerTwo.getName();
+                    break;
+                }
+                if(board[0][0]!=null && board[0][0]==board[1][1] && board[1][1]==board[2][2]) {
+                    winner= turn?playerOne.getName():playerTwo.getName();
+                    break;
+                }
+                if(board[0][2]!=null && board[0][2]==board[1][1] && board[1][1]==board[2][0]) {
+                    winner= turn?playerOne.getName():playerTwo.getName();
+                    break;
+                }
+            }
+            turn=!turn;
+            if(!winner.isEmpty()) {
+                break;
+            }
+        }
+        System.out.println("Winner : "+winner);
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(board[i][j]!=null) {
+                    System.out.print(board[i][j]+" ");
+                }
+                else {
+                    System.out.print("-"+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
