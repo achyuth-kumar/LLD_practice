940abcc56b67f660a5ceb39429e64cf610b08e83
achyuth-kumar <achyuthkumar773@gmail.com>
2026-07-26 06:53:44 +0530

    26-7-2026

---
+++ Spotify19/Song.java
@@ -0,0 +1,28 @@
+public class Song {
+    String name;
+    Integer id;
+
+    public Song(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+}
