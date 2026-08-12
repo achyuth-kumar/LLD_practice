015be803d8b3a952e0023e8108a8c1afa0253a73
achyuth-kumar <achyuthkumar773@gmail.com>
2026-08-12 07:48:55 +0530

    12-8-2026

---
+++ ChainOfResponsibility36/TwoThousandHandler.java
@@ -0,0 +1,20 @@
+public class TwoThousandHandler extends Operations{
+    public TwoThousandHandler(Operations next) {
+        super(next);
+    }
+
+    @Override
+    public void processTransaction(Integer amount) {
+        if(amount>=2000) {
+            Integer notes=amount/2000;
+            Integer remainder=amount%2000;
+            System.out.println("Two Thousand Notes : "+notes);
+            if(remainder!=0 && next!=null) {
+                next.processTransaction(remainder);
+            }
+        }
+        else {
+            next.processTransaction(amount);
+        }
+    }
+}
