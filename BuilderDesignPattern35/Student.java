eb6b94e013c49998582f44ffdb444954a385fae0
achyuth-kumar <achyuthkumar773@gmail.com>
2026-09-22 07:06:20 +0530

    22-9-2026

---
+++ BuilderDesignPattern35/Student.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class Student {
+    Integer id;
+    String firstName,lastName,email;
+    List<String> subjects;
+    public Student(StudentBuilder studentBuilder) {
+        this.id = studentBuilder.getId();
+        this.firstName = studentBuilder.getFirstName();
+        this.lastName = studentBuilder.getLastName();
+        this.email = studentBuilder.getEmail();
+        this.subjects = studentBuilder.getSubjects();
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<String> getSubjects() {
+        return subjects;
+    }
+
+
+
+}
