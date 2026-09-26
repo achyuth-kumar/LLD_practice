2af08d4f8136605e98a02fcf5e262b18c274c6ce
achyuth-kumar <achyuthkumar773@gmail.com>
2026-09-26 10:19:45 +0530

    26-9-2026

---
+++ IteratorDesignPattern21/TextBook.java
@@ -0,0 +1,29 @@
+public class TextBook {
+    public Integer id;
+    public String title;
+
+    public TextBook(Integer id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+
+
+
+}
